2300c6a6e28aafc9313de6d4ae5d26586c3aa499
yeah <qi.school.eml@gmail.com>
2026-03-12 13:37:27 -0400

    Update placeholder_js.js

--- placeholder_js.js
+++ placeholder_js.js
@@ -18,25 +18,51 @@ function getOffset(el) {
   
 //
 
-function oksigma() {
-    console.log("AAH?")
+let words = [];
+function oksigma(inc) {
     let copy = text_id.cloneNode(true);
     trail_body.appendChild(copy);
     copy.style.animation = "thefade 1s linear lalalalala 5s linear";
     copy.style.position = "relative";
-    
-    let offset = getOffset(copy);
 
-    // memory leak fix, but bigs the site, so..not sorry! :3
+    //table.insert(words, copy);
+    words.push(copy);
 
-    // window.setTimeout(function() {
-    //     trail_body.removeChild(copy);
-    //     console.log("eaa?")
-    // }, 5000)
+    // let mod_inc = (inc * 0.1) % 100;
+    // console.log(mod_inc);
+    // copy.style.color = `hsl(${mod_inc} 100 50)`;
+
+    // copy.style.color = "green";
 }
+
 let time_to_wait = 0;
+
 for (let i = 0; i < 100; i++) {
     time_to_wait += 25;
-    console.log(time_to_wait);
-    window.setTimeout(oksigma, time_to_wait);
+    let counter = time_to_wait;
+    window.setTimeout(()=>{oksigma(counter)}, time_to_wait);
 }
+
+let dt_a = Date.now();
+let elapsed = 0;
+let secondary_wait = 0;
+
+setInterval(() => {
+  dt = Date.now() - dt_a;
+  dt_a = Date.now();
+
+  elapsed += dt * 0.15;
+
+  text_id.style.color = `hsl(${elapsed} 50 50)`;
+  for (let index = 0; index < words.length; index++) {
+    
+    secondary_wait += 25;
+    let counter = secondary_wait;
+    window.setTimeout(()=>{
+      element = words[index];
+      element.style.color = `hsl(${elapsed} 50 50)`;
+    }, secondary_wait)
+  }
+
+  // for _, v in words do v.style.color = `hsl(${elapsed} 100 50)` end
+}, 100);
