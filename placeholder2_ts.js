1b38529ecaec5a2ce5fd77d10d6e3e3d3fe0a3f1
yeah <qi.school.eml@gmail.com>
2026-03-17 13:00:01 -0400

    changed a bunch of things ye
    w

---
+++ placeholder2_ts.js
@@ -0,0 +1,40 @@
+var body = document.getElementById("body");
+function create_text(text, parent, callback) {
+    var parent_div = document.createElement("div");
+    var text_array = [];
+    for (var _i = 0, text_1 = text; _i < text_1.length; _i++) {
+        var text_string = text_1[_i];
+        if (text_string == " ") {
+            text_string = "\u00a0";
+        }
+        var element = document.createElement("span");
+        element.textContent = text_string;
+        parent_div.appendChild(element);
+        text_array.push(element);
+    }
+    parent.appendChild(parent_div);
+    if (callback) {
+        callback(parent_div, text_array);
+    }
+}
+var awesome_div = document.createElement("div");
+body.appendChild(awesome_div);
+create_text("lalalalal :3 :3 :3 :3", awesome_div, function (parent, text) {
+    parent.style.display = "flex";
+    parent.style.alignItems = "center";
+    parent.style.position = "absolute";
+    parent.style.right = "50%";
+    var inc_wait = 0;
+    var _loop_1 = function (index) {
+        inc_wait += 25;
+        var element = text[index];
+        window.setTimeout(function () {
+            element.style.position = "relative";
+            element.style.animation = "wowie 5s cubic-bezier(0.45, 0, 0.55, 1) infinite";
+            console.log(text[index]);
+        }, inc_wait);
+    };
+    for (var index = 0; index < text.length; index++) {
+        _loop_1(index);
+    }
+});
