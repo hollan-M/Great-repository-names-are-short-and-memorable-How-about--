b51612a42086c261926e47c878aec1c3f3ec3281
yeah <qi.school.eml@gmail.com>
2026-03-17 13:33:00 -0400

    Update placeholder2_ts.js

--- placeholder2_ts.js
+++ placeholder2_ts.js
@@ -7,7 +7,7 @@ function create_text(text, parent, callback) {
         if (text_string == " ") {
             text_string = "\u00a0";
         }
-        var element = document.createElement("span");
+        var element = document.createElement("a");
         element.textContent = text_string;
         parent_div.appendChild(element);
         text_array.push(element);
@@ -17,13 +17,13 @@ function create_text(text, parent, callback) {
         callback(parent_div, text_array);
     }
 }
-var awesome_div = document.createElement("div");
-body.appendChild(awesome_div);
-create_text("lalalalal :3 :3 :3 :3", awesome_div, function (parent, text) {
+var quote_div = document.createElement("div");
+body.appendChild(quote_div);
+create_text("QUOTES!! Rene descartes or something like that", quote_div, function (parent, text) {
     parent.style.display = "flex";
     parent.style.alignItems = "center";
     parent.style.position = "absolute";
-    parent.style.right = "50%";
+    parent.style.left = "5%";
     var inc_wait = 0;
     var _loop_1 = function (index) {
         inc_wait += 25;
@@ -36,5 +36,28 @@ create_text("lalalalal :3 :3 :3 :3", awesome_div, function (parent, text) {
     };
     for (var index = 0; index < text.length; index++) {
         _loop_1(index);
+    }
+});
+var box_div = document.createElement("div");
+body.appendChild(box_div);
+create_text("BOXES!! Check this swag out oh my goodness!!", box_div, function (parent, text) {
+    parent.style.display = "flex";
+    parent.style.alignItems = "center";
+    parent.style.position = "absolute";
+    parent.style.left = "5%";
+    parent.style.top = "25%";
+    var inc_wait = 0;
+    var _loop_2 = function (index) {
+        inc_wait += 25;
+        var element = text[index];
+        element.href = "https://hollan-m.github.io/Great-repository-names-are-short-and-memorable-How-about--/boxes/boxes.html";
+        window.setTimeout(function () {
+            element.style.position = "relative";
+            element.style.animation = "wowie 5s cubic-bezier(0.45, 0, 0.55, 1) infinite";
+            console.log(text[index]);
+        }, inc_wait);
+    };
+    for (var index = 0; index < text.length; index++) {
+        _loop_2(index);
     }
 });
