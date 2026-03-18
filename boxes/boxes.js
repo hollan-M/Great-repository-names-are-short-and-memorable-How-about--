84db77776902bbfb9e3ed43cc775714bf3089690
yeah <qi.school.eml@gmail.com>
2026-03-18 13:50:44 -0400

    fixed boxes
    sad yeh

--- boxes/boxes.js
+++ boxes/boxes.js
@@ -1,1 +1,37 @@
-console.log("A");
+var body = document.getElementById("body");
+function create_text(text, parent, callback) {
+    var parent_div = document.createElement("div");
+    var text_array = [];
+    for (var _i = 0, text_1 = text; _i < text_1.length; _i++) {
+        var text_string = text_1[_i];
+        if (text_string == " ") {
+            text_string = "\u00a0";
+        }
+        var element = document.createElement("span");
+        element.textContent = text_string;
+        parent_div.appendChild(element);
+        text_array.push(element);
+    }
+    parent.appendChild(parent_div);
+    if (callback) {
+        callback(parent_div, text_array);
+    }
+}
+var box_1 = document.getElementById("box1");
+create_text("Wow! This is a MAGIC BOX!", box_1, function (parent, text) {
+    var inc_wait = 0;
+    var _loop_1 = function (element) {
+        inc_wait += 25;
+        window.setTimeout(function () {
+            console.log(element);
+            element.style.display = "inline-block";
+            element.style.position = "relative";
+            element.style.animation = "boxtext1 5s cubic-bezier(0.45, 0, 0.55, 1) infinite";
+        }, inc_wait);
+    };
+    for (var _i = 0, text_2 = text; _i < text_2.length; _i++) {
+        var element = text_2[_i];
+        _loop_1(element);
+    }
+});
+console.log("??");
--- boxes/boxes.ts
+++ boxes/boxes.ts
@@ -1,1 +1,36 @@
-console.log("A");
+const body = document.getElementById("body")!;
+function create_text(text: string, parent: HTMLElement, callback: (parent_div: HTMLDivElement, text_array: HTMLSpanElement[]) => void) {
+    const parent_div = document.createElement("div");
+
+    const text_array = [] as HTMLSpanElement[];
+
+    for (let text_string of text) {
+        if (text_string == " ") {text_string = "\u00a0"}
+        const element = document.createElement("span");
+        element.textContent = text_string;
+        parent_div.appendChild(element);
+        text_array.push(element);
+    }
+
+    parent.appendChild(parent_div);
+
+    if (callback) {
+        callback(parent_div, text_array);
+    }
+}
+
+const box_1 = document.getElementById("box1");
+create_text("Wow! This is a MAGIC BOX!", box_1 as HTMLDivElement, (parent: HTMLDivElement, text: HTMLSpanElement[])=>{
+    let inc_wait = 0;
+    for (const element of text) {
+        inc_wait += 25;
+        window.setTimeout(()=>{
+            console.log(element);
+            element.style.display = "inline-block";
+            element.style.position = "relative";
+            element.style.animation = "boxtext1 5s cubic-bezier(0.45, 0, 0.55, 1) infinite"
+        }, inc_wait)
+    }
+});
+
+console.log("??")
